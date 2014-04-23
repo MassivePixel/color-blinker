8121f642907afdc1043ffe481bc4f1ed9dca2ec5
Toni Petrina <toni@massivepixel.hr>
2014-04-23 23:09:09 +0200

    Adding project skeleton

---
+++ MassivePixel.ColorMe.WP8/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MassivePixel.ColorMe.WP8.Views
+{
+    public partial class MainPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
